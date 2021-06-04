8625bbf96077016ab4284314abbb33e08dfc18db
gabrielle <gabrielle.kbidi@gmail.com>
2021-06-04 10:59:58 +0200

    challenge ok

--- Eagle.java
+++ Eagle.java
@@ -1,6 +1,7 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
-    private boolean flying;
+
+	private boolean flying;
     private int altitude;
 
     public Eagle(String name) {
@@ -9,16 +10,62 @@ public class Eagle extends Bird {
         this.altitude = 0;
     }
 
-    public int getAltitude() {
-        return altitude;
-    }
+   
 
     public boolean isFlying() {
         return flying;
     }
+    
+    public void setFlying(boolean flying) {
+		this.flying = flying;
+	}
+
+	public void setAltitude(int altitude) {
+		this.altitude = altitude;
+	}
 
     @Override
     public String sing() {
         return "Screech!";
     }
+
+	@Override
+	public void takeOff() {
+			System.out.println(this.getName() + " is already flying.");
+		}
+	
+
+	@Override
+	public void land() {
+		// TODO Auto-generated method stub
+		System.out.println(this.getName() + " lands on the ground");
+	}
+
+	@Override
+	public void ascend(int meters) {
+		// TODO Auto-generated method stub
+		this.setAltitude(this.altitude + meters);
+		System.out.println(getName() + " flies upward, altitude : " + this.altitude );
+	}
+
+	@Override
+	public void descend(int meters) {
+		// TODO Auto-generated method stub
+		this.setAltitude(this.altitude - meters);
+		System.out.println(getName() + " flies downward, altitude : " + this.altitude );
+	}
+
+	@Override
+	public void glide() {
+		// TODO Auto-generated method stub
+		System.out.println(getName() + " glides into the air");
+	}
+
+
+
+	@Override
+	public int getAltitude() {
+		// TODO Auto-generated method stub
+		return 400;
+	}
 }
---
+++ Fly.java
@@ -0,0 +1,16 @@
+
+public interface Fly {
+	
+	public void takeOff();
+	
+	public void ascend(int meters);
+	
+	public void descend(int meters);
+	
+	public void land();
+	
+	public void glide();
+	
+	public int getAltitude();
+
+}
